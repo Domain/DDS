49ff37663800f3e182c54fa4406a7ab8f979954e
Domain <do.main@msn.com>
2013-01-26 17:53:07 +0800

    update

--- tools/main.d
+++ tools/main.d
@@ -11,190 +11,190 @@ import std.algorithm;
 
 void generateAllFile(string dir, string prefix)
 {
-	enforce(dir.isDir());
-	chdir(dir);
-	auto path = relativePath(dir, prefix);
-	path = path.replace("\\", ".");
-	auto files = appender!string();
-	foreach (entry; dirEntries(dir, SpanMode.shallow)) 
-	{ 
-		if (entry.isFile() && entry.name.extension() == ".d")
-		{
-			auto filename = baseName(entry.name, ".d");
+    enforce(dir.isDir());
+    chdir(dir);
+    auto path = relativePath(dir, prefix);
+    path = path.replace("\\", ".");
+    auto files = appender!string();
+    foreach (entry; dirEntries(dir, SpanMode.shallow)) 
+    { 
+        if (entry.isFile() && entry.name.extension() == ".d")
+        {
+            auto filename = baseName(entry.name, ".d");
 
-			if (filename != "all")
-				files.put("public import " ~ path ~ "." ~ filename ~";\n");
-		}
-	}
-	if (files.data != "")
-	{
-		auto f = File("all.d", "w");
-		f.writeln("module " ~ path ~ ".all;\n");
-		f.write(files.data);
-		f.close();
-	}
+            if (filename != "all")
+                files.put("public import " ~ path ~ "." ~ filename ~";\n");
+        }
+    }
+    if (files.data != "")
+    {
+        auto f = File("all.d", "w");
+        f.writeln("module " ~ path ~ ".all;\n");
+        f.write(files.data);
+        f.close();
+    }
 }
 
 void generateMissingFiles(string filename, string base)
 {
-	auto content = std.file.readText(filename);
+    auto content = std.file.readText(filename);
 
-	auto modR = regex(`^\s*import\s*([_a-zA-Z][_a-zA-Z0-9]*(?:\.[_a-zA-Z][_a-zA-Z0-9]*)*)\s*;\s*$`, "gm");
-	foreach (c; match(content, modR))
-	{
-		string modName = c[1];
-		writeln(modName);
-		string modFile = buildPath(base, modName.replace(".", dirSeparator)) ~ ".d";
-		if (!modFile.exists)
-		{
-			if (!dirName(modFile).exists)
-				dirName(modFile).mkdirRecurse();
-			auto className = modFile.baseName().stripExtension();
-			auto app = appender!string();
-			app.put("/// Generate by tools\n");
-			app.put("module " ~ modName ~ ";\n");
-			app.put("\n");
-			app.put("import java.lang.exceptions;\n");
-			app.put("\n");
-			app.put("public class " ~ className ~ "\n");
-			app.put("{\n");
-			app.put("    public this()\n");
-			app.put("    {\n");
-			app.put("        implMissing();\n");
-			app.put("    }\n");
-			app.put("}\n");
-			std.file.write(modFile, app.data);
-		}
-	}
+    auto modR = regex(`^\s*import\s*([_a-zA-Z][_a-zA-Z0-9]*(?:\.[_a-zA-Z][_a-zA-Z0-9]*)*)\s*;\s*$`, "gm");
+    foreach (c; match(content, modR))
+    {
+        string modName = c[1];
+        writeln(modName);
+        string modFile = buildPath(base, modName.replace(".", dirSeparator)) ~ ".d";
+        if (!modFile.exists)
+        {
+            if (!dirName(modFile).exists)
+                dirName(modFile).mkdirRecurse();
+            auto className = modFile.baseName().stripExtension();
+            auto app = appender!string();
+            app.put("/// Generate by tools\n");
+            app.put("module " ~ modName ~ ";\n");
+            app.put("\n");
+            app.put("import java.lang.exceptions;\n");
+            app.put("\n");
+            app.put("public class " ~ className ~ "\n");
+            app.put("{\n");
+            app.put("    public this()\n");
+            app.put("    {\n");
+            app.put("        implMissing();\n");
+            app.put("    }\n");
+            app.put("}\n");
+            std.file.write(modFile, app.data);
+        }
+    }
 }
 
 void j2d(string filename, string jdir, string ddir)
 {
-	auto rpath = relativePath(filename, jdir);
-	auto dname = ddir ~ rpath;
+    auto rpath = relativePath(filename, jdir);
+    auto dname = ddir ~ rpath;
 
-	if (!dirName(dname).exists)
-		dirName(dname).mkdirRecurse();
+    if (!dirName(dname).exists)
+        dirName(dname).mkdirRecurse();
 
-	if (filename.extension() != ".java")
-	{
-		copy(filename, dname);
-		return;
-	}
+    if (filename.extension() != ".java")
+    {
+        copy(filename, dname);
+        return;
+    }
 
-	auto src = File(filename, "r");
-	scope(exit) src.close();
-	auto content = appender!string();
-	foreach (line; src.byLine(KeepTerminator.yes))
-	{
-		auto newLine = line;
-		if (newLine.startsWith("package "))
-		{
-			newLine = newLine.replace("package ", "module ");
-			newLine = newLine.replace(";", "." ~ baseName(filename, ".java") ~ ";");
-		}
-		newLine = newLine.replace(" extends ", " : ");
-		newLine = newLine.replace(" implements ", " : ");
-		newLine = newLine.replace("@Override", "override");
-		newLine = newLine.replace("== null", "is null");
-		newLine = newLine.replace("!= null", "!is null");
-		newLine = newLine.replace("boolean", "bool");
-		newLine = newLine.replace("@Deprecated", "deprecated");
-		newLine = newLine.replace(".debug(", ".debug_(");
-		content.put(newLine);
-	}
+    auto src = File(filename, "r");
+    scope(exit) src.close();
+    auto content = appender!string();
+    foreach (line; src.byLine(KeepTerminator.yes))
+    {
+        auto newLine = line;
+        if (newLine.startsWith("package "))
+        {
+            newLine = newLine.replace("package ", "module ");
+            newLine = newLine.replace(";", "." ~ baseName(filename, ".java") ~ ";");
+        }
+        newLine = newLine.replace(" extends ", " : ");
+        newLine = newLine.replace(" implements ", " : ");
+        newLine = newLine.replace("@Override", "override");
+        newLine = newLine.replace("== null", "is null");
+        newLine = newLine.replace("!= null", "!is null");
+        newLine = newLine.replace("boolean", "bool");
+        newLine = newLine.replace("@Deprecated", "deprecated");
+        newLine = newLine.replace(".debug(", ".debug_(");
+        content.put(newLine);
+    }
 
-	auto text = content.data;
+    auto text = content.data;
 
-	auto ctorR = regex(`(\s*)(public|private|protected)(\s+)[_a-zA-Z][_a-zA-Z0-9]*(\s*)\(`, "g");
-	text = std.regex.replace(text, ctorR, "$1$2$3this$4(");
+    auto ctorR = regex(`(\s*)(public|private|protected)(\s+)[_a-zA-Z][_a-zA-Z0-9]*(\s*)\(`, "g");
+    text = std.regex.replace(text, ctorR, "$1$2$3this$4(");
 
-	auto throwR = regex(`(\))\s*throws\s+[_a-zA-Z][_a-zA-Z0-9]*(?:\s*,\s*[_a-zA-Z][_a-zA-Z0-9]*)*(\s*)(\{|;)`, "g");
-	text = std.regex.replace(text, throwR, "$1$2$3");
+    auto throwR = regex(`(\))\s*throws\s+[_a-zA-Z][_a-zA-Z0-9]*(?:\s*,\s*[_a-zA-Z][_a-zA-Z0-9]*)*(\s*)(\{|;)`, "g");
+    text = std.regex.replace(text, throwR, "$1$2$3");
 
-	auto subClassR = regex(`new\s+([_a-zA-Z][_a-zA-Z0-9]*)\s*(\([^\)]*\))\s*\{`, "g");
-	text = std.regex.replace(text, subClassR, "new class$2 $1 {");
+    auto subClassR = regex(`new\s+([_a-zA-Z][_a-zA-Z0-9]*)\s*(\([^\)]*\))\s*\{`, "g");
+    text = std.regex.replace(text, subClassR, "new class$2 $1 {");
 
-	auto hashR = regex(`\bint\b(\s*)hashCode(\s*\()`, "g");
-	text = std.regex.replace(text, hashR, "override hash_t$1toHash$2");
+    auto hashR = regex(`\bint\b(\s*)hashCode(\s*\()`, "g");
+    text = std.regex.replace(text, hashR, "override hash_t$1toHash$2");
 
-	auto equalsR = regex(`\bbool\b(\s*)equals(\s*\()`, "g");
-	text = std.regex.replace(text, equalsR, "override equals_t$1opEquals$2");
+    auto equalsR = regex(`\bbool\b(\s*)equals(\s*\()`, "g");
+    text = std.regex.replace(text, equalsR, "override equals_t$1opEquals$2");
 
-	auto instanceofR = regex(`([\(,|&=])\s*([_a-zA-Z][\._a-zA-Z0-9\(\)]*)\s+instanceof\s+([_a-zA-Z][_a-zA-Z0-9]*)(\s*[),|&?])`, "g");
-	text = std.regex.replace(text, instanceofR, "$1 cast($3)$2 !is null $4");
+    auto instanceofR = regex(`([\(,|&=])\s*([_a-zA-Z][\._a-zA-Z0-9\(\)]*)\s+instanceof\s+([_a-zA-Z][_a-zA-Z0-9]*)(\s*[),|&?])`, "g");
+    text = std.regex.replace(text, instanceofR, "$1 cast($3)$2 !is null $4");
 
-	auto outerR = regex(`\b[_a-zA-Z][_a-zA-Z0-9]*\.this\b`, "g");
-	text = std.regex.replace(text, outerR, "this.outer");
+    auto outerR = regex(`\b[_a-zA-Z][_a-zA-Z0-9]*\.this\b`, "g");
+    text = std.regex.replace(text, outerR, "this.outer");
 
-	auto arrayR = regex(`new\s+([_a-zA-Z][\._a-zA-Z0-9]*\s*\[\s*\])\s*\{([^\}]*)\}`, "g");
-	text = std.regex.replace(text, arrayR, "cast($1)[$2]");
+    auto arrayR = regex(`new\s+([_a-zA-Z][\._a-zA-Z0-9]*\s*\[\s*\])\s*\{([^\}]*)\}`, "g");
+    text = std.regex.replace(text, arrayR, "cast($1)[$2]");
 
-	auto templDeclR = regex(`(class|interface)(\s+)([_a-zA-Z][_a-zA-Z0-9]*)<(\s*(?:[_a-zA-Z][_a-zA-Z0-9]*)(?:\s*,\s*[_a-zA-Z][_a-zA-Z0-9]*)*(?:\s+:\s+[_a-zA-Z][_a-zA-Z0-9]*)?\s*)*>`, "g");
-	text = std.regex.replace(text, templDeclR, "$1$2$3($4)");
+    auto templDeclR = regex(`(class|interface)(\s+)([_a-zA-Z][_a-zA-Z0-9]*)<(\s*(?:[_a-zA-Z][_a-zA-Z0-9]*)(?:\s*,\s*[_a-zA-Z][_a-zA-Z0-9]*)*(?:\s+:\s+[_a-zA-Z][_a-zA-Z0-9]*)?\s*)*>`, "g");
+    text = std.regex.replace(text, templDeclR, "$1$2$3($4)");
 
-	//auto templR = regex(`<(\s*[_a-zA-Z\?][\.,0-9_a-zA-Z\s\?]*\s*)>`, "g");
-	//text = std.regex.replace(text, templR, "!($1)");
-	//
-	//auto templR2 = regex(`<(\s*[_a-zA-Z\?][\.,0-9_a-zA-Z!\(\)\s\?]*(?::[_a-zA-Z][\.,0-9_a-zA-Z]*)\s*)>`, "g");
-	//text = std.regex.replace(text, templR2, "!($1)");
+    //auto templR = regex(`<(\s*[_a-zA-Z\?][\.,0-9_a-zA-Z\s\?]*\s*)>`, "g");
+    //text = std.regex.replace(text, templR, "!($1)");
+    //
+    //auto templR2 = regex(`<(\s*[_a-zA-Z\?][\.,0-9_a-zA-Z!\(\)\s\?]*(?::[_a-zA-Z][\.,0-9_a-zA-Z]*)\s*)>`, "g");
+    //text = std.regex.replace(text, templR2, "!($1)");
 
-	auto templR1 = regex(`<([_a-zA-Z0-9\s:!\(\)\?,\.]+?)>`, "g");
-	text = std.regex.replace(text, templR1, "!($1)");
+    auto templR1 = regex(`<([_a-zA-Z0-9\s:!\(\)\?,\.]+?)>`, "g");
+    text = std.regex.replace(text, templR1, "!($1)");
 
-	auto templR2 = regex(`<([_a-zA-Z0-9\s:!\(\)\?,\.]+?)>`, "g");
-	text = std.regex.replace(text, templR2, "!($1)");
+    auto templR2 = regex(`<([_a-zA-Z0-9\s:!\(\)\?,\.]+?)>`, "g");
+    text = std.regex.replace(text, templR2, "!($1)");
 
-	auto templR3 = regex(`<([_a-zA-Z0-9\s:!\(\)\?,\.]+?)>`, "g");
-	text = std.regex.replace(text, templR3, "!($1)");
+    auto templR3 = regex(`<([_a-zA-Z0-9\s:!\(\)\?,\.]+?)>`, "g");
+    text = std.regex.replace(text, templR3, "!($1)");
 
-	auto loggerR = regex(`\.getLogger\(\s*(.+)\.class\s*\)`, "g");
-	text = std.regex.replace(text, loggerR, ".getLogger!($1)");
+    auto loggerR = regex(`\.getLogger\(\s*(.+)\.class\s*\)`, "g");
+    text = std.regex.replace(text, loggerR, ".getLogger!($1)");
 
-	auto classR = regex(`\.class\b`, "g");
-	text = std.regex.replace(text, classR, ".class_");
+    auto classR = regex(`\.class\b`, "g");
+    text = std.regex.replace(text, classR, ".class_");
 
-	auto deleteR = regex(`\bdelete\(`, "g");
-	text = std.regex.replace(text, deleteR, "delete_(");
+    auto deleteR = regex(`\bdelete\(`, "g");
+    text = std.regex.replace(text, deleteR, "delete_(");
 
-	auto staticThisR = regex(`static(\s*)\{`, "g");
-	text = std.regex.replace(text, staticThisR, "static this()$1{");
+    auto staticThisR = regex(`static(\s*)\{`, "g");
+    text = std.regex.replace(text, staticThisR, "static this()$1{");
 
-	auto foreachR = regex(`for(\s*)\(([_a-zA-Z][_a-zA-Z0-9!\(\),\s\[\]\?]*\s+[_a-zA-Z][_a-zA-Z0-9]*\s*):(\s*[_a-zA-Z][_a-zA-Z0-9\.!\(\),\s\[\]]*)\)`, "g");
-	text = std.regex.replace(text, foreachR, "foreach$1($2;$3)");
+    auto foreachR = regex(`for(\s*)\(([_a-zA-Z][_a-zA-Z0-9!\(\),\s\[\]\?]*\s+[_a-zA-Z][_a-zA-Z0-9]*\s*):(\s*[_a-zA-Z][_a-zA-Z0-9\.!\(\),\s\[\]]*)\)`, "g");
+    text = std.regex.replace(text, foreachR, "foreach$1($2;$3)");
 
-	auto questionR = regex(`!\([^\)]*\?[^\)]*\)`, "g");
-	text = std.regex.replace(text, questionR, "/*$&*/");
+    auto questionR = regex(`!\([^\)]*\?[^\)]*\)`, "g");
+    text = std.regex.replace(text, questionR, "/*$&*/");
 
-	auto newCallR = regex(`(new\s+[_a-zA-Z][_a-zA-Z0-9]*\((?:[_a-zA-Z0-9\-]*)?\))\.`, "g");
-	text = std.regex.replace(text, newCallR, "($1).");
+    auto newCallR = regex(`(new\s+[_a-zA-Z][_a-zA-Z0-9]*\((?:[_a-zA-Z0-9\-]*)?\))\.`, "g");
+    text = std.regex.replace(text, newCallR, "($1).");
 
-	auto digitR = regex(`(\d+\.\d+)D`, "g");
-	text = std.regex.replace(text, digitR, "$1");
+    auto digitR = regex(`(\d+\.\d+)D`, "g");
+    text = std.regex.replace(text, digitR, "$1");
 
-	auto multiR = regex(`class(\s+[_a-zA-Z][_a-zA-Z0-9]*(?:\(.+\))?\s*):(\s*[_a-zA-Z][_a-zA-Z0-9]*(?:!\([_a-zA-Z][_a-zA-Z0-9]*(?:\s*,\s*[_a-zA-Z][_a-zA-Z0-9]*)?\))?\s*):(\s*[_a-zA-Z][_a-zA-Z0-9]*)`, "g");
-	text = std.regex.replace(text, multiR, "class$1:$2,$3");
+    auto multiR = regex(`class(\s+[_a-zA-Z][_a-zA-Z0-9]*(?:\(.+\))?\s*):(\s*[_a-zA-Z][_a-zA-Z0-9]*(?:!\([_a-zA-Z][_a-zA-Z0-9]*(?:\s*,\s*[_a-zA-Z][_a-zA-Z0-9]*)?\))?\s*):(\s*[_a-zA-Z][_a-zA-Z0-9]*)`, "g");
+    text = std.regex.replace(text, multiR, "class$1:$2,$3");
 
-	auto castR = regex(`(return\s+|[\(,|&=:])(\s*)(\(\s*(?:[_a-zA-Z][_a-zA-Z0-9]*(?:\.[_a-zA-Z][_a-zA-Z0-9]*)?)\s*(?:\[\s*\]\s*)?(?:!\([_a-zA-Z][_a-zA-Z0-9]*\))?\))(\s*[_a-zA-Z])`, "g");
-	text = std.regex.replace(text, castR, "$1$2cast$3$4");
+    auto castR = regex(`(return\s+|[\(,|&=:])(\s*)(\(\s*(?:[_a-zA-Z][_a-zA-Z0-9]*(?:\.[_a-zA-Z][_a-zA-Z0-9]*)?)\s*(?:\[\s*\]\s*)?(?:!\([_a-zA-Z][_a-zA-Z0-9]*\))?\))(\s*[_a-zA-Z])`, "g");
+    text = std.regex.replace(text, castR, "$1$2cast$3$4");
 
-	auto warningR = regex(`@SuppressWarnings\(".*"\)`, "g");
-	text = std.regex.replace(text, warningR, "");
+    auto warningR = regex(`@SuppressWarnings\(".*"\)`, "g");
+    text = std.regex.replace(text, warningR, "");
 
-	auto volatileR = regex(`\bvolatile\b`, "g");
-	text = std.regex.replace(text, volatileR, "/*volatile*/");
+    auto volatileR = regex(`\bvolatile\b`, "g");
+    text = std.regex.replace(text, volatileR, "/*volatile*/");
 
-	// Bug for jd-gui
-	auto iDollarR = regex(`i\$`, "g");
-	text = std.regex.replace(text, iDollarR, "i");
+    // Bug for jd-gui
+    auto iDollarR = regex(`i\$`, "g");
+    text = std.regex.replace(text, iDollarR, "i");
 
-	auto enumR = regex(`(enum\s+[_a-zA-Z][_a-zA-Z0-9]*\s*\{\s*[_a-zA-Z][_a-zA-Z0-9]*(?:\s*,\s*[_a-zA-Z][_a-zA-Z0-9]*)*);(\s*\})`, "g");
-	text = std.regex.replace(text, enumR, "$1$2");
+    auto enumR = regex(`(enum\s+[_a-zA-Z][_a-zA-Z0-9]*\s*\{\s*[_a-zA-Z][_a-zA-Z0-9]*(?:\s*,\s*[_a-zA-Z][_a-zA-Z0-9]*)*);(\s*\})`, "g");
+    text = std.regex.replace(text, enumR, "$1$2");
 
-	dname = dname.replace(".java", ".d");
-	std.file.write(dname, text);
+    dname = dname.replace(".java", ".d");
+    std.file.write(dname, text);
 
-	return;
+    return;
 }
 
 void printHelp()
@@ -203,102 +203,102 @@ void printHelp()
 
 int main(string[] argv)
 {
-	string jdir;
-	string ddir;
+    string jdir;
+    string ddir;
 
-	bool convert = false;
-	bool genAll = false;
-	bool genMissing = false;
-	bool showHelp = false;
+    bool convert = false;
+    bool genAll = false;
+    bool genMissing = false;
+    bool showHelp = false;
 
-	try
-	{
-		getopt(argv, 
-			   "javadir|j", &jdir,
-			   "ddir|d", &ddir,
-			   std.getopt.config.bundling,
-			   "convert|c", &convert,
-			   "genAll|a", &genAll,
-			   "genMissing|m", &genMissing,
-			   "help|h", &showHelp);
-	}
-	catch(Exception e)
-	{
-		writeln(e.msg);
-	}
+    try
+    {
+        getopt(argv, 
+               "javadir|j", &jdir,
+               "ddir|d", &ddir,
+               std.getopt.config.bundling,
+               "convert|c", &convert,
+               "genAll|a", &genAll,
+               "genMissing|m", &genMissing,
+               "help|h", &showHelp);
+    }
+    catch(Exception e)
+    {
+        writeln(e.msg);
+    }
 
-	if (argv.length > 1)
-	{
-		writeln("Unrecognized option: ", argv[1..$]);
-		return 1;
-	}
+    if (argv.length > 1)
+    {
+        writeln("Unrecognized option: ", argv[1..$]);
+        return 1;
+    }
 
-	if (!convert && !genAll && !genMissing)
-	{
-		writeln("At least specify one option: convert(c), genAll(a), genMissing(m)");
-		return 2;
-	}
+    if (!convert && !genAll && !genMissing)
+    {
+        writeln("At least specify one option: convert(c), genAll(a), genMissing(m)");
+        return 2;
+    }
 
-	if (convert && (jdir == "" || ddir == ""))
-	{
-		writeln("Missing dir.");
-		printHelp();
-		return 2;
-	}
+    if (convert && (jdir == "" || ddir == ""))
+    {
+        writeln("Missing dir.");
+        printHelp();
+        return 2;
+    }
 
-	if (genAll && ddir == "")
-	{
-		writeln("Missing dir.");
-		printHelp();
-		return 2;
-	}
+    if (genAll && ddir == "")
+    {
+        writeln("Missing dir.");
+        printHelp();
+        return 2;
+    }
 
-	if (genMissing && ddir == "")
-	{
-		writeln("Missing dir.");
-		printHelp();
-		return 2;
-	}
+    if (genMissing && ddir == "")
+    {
+        writeln("Missing dir.");
+        printHelp();
+        return 2;
+    }
 
-	if (!jdir.endsWith(dirSeparator))
-		jdir ~= dirSeparator;
+    if (!jdir.endsWith(dirSeparator))
+        jdir ~= dirSeparator;
 
-	if (!ddir.endsWith(dirSeparator))
-		ddir ~= dirSeparator;
+    if (!ddir.endsWith(dirSeparator))
+        ddir ~= dirSeparator;
 
-	if (convert)
-	{
-		foreach (entry; dirEntries(jdir, SpanMode.depth)) 
-		{ 
-			if (entry.isFile())
-			{
-				writeln("processing " ~ entry.name ~ " ...");
-				j2d(entry.name, jdir, ddir);
-			}
-		}
-	}
+    if (convert)
+    {
+        foreach (entry; dirEntries(jdir, SpanMode.depth)) 
+        { 
+            if (entry.isFile())
+            {
+                writeln("processing " ~ entry.name ~ " ...");
+                j2d(entry.name, jdir, ddir);
+            }
+        }
+    }
 
-	if (genAll)
-	{
-		foreach (entry; dirEntries(ddir, SpanMode.depth)) 
-		{ 
-			if (entry.isDir())
-			{
-				generateAllFile(entry.name, ddir);
-			}
-		}
-	}
+    if (genAll)
+    {
+        foreach (entry; dirEntries(ddir, SpanMode.depth)) 
+        { 
+            if (entry.isDir())
+            {
+                generateAllFile(entry.name, ddir);
+            }
+        }
+    }
 
-	if (genMissing)
-	{
-		foreach (entry; dirEntries(ddir, SpanMode.depth)) 
-		{ 
-			if (entry.isFile() && entry.name.extension() == ".d")
-			{
-				generateMissingFiles(entry.name, ddir);
-			}
-		}
-	}
+    if (genMissing)
+    {
+        foreach (entry; dirEntries(ddir, SpanMode.depth)) 
+        { 
+            if (entry.isFile() && entry.name.extension() == ".d")
+            {
+                generateMissingFiles(entry.name, ddir);
+            }
+        }
+    }
 
-	return 0;
+    return 0;
 }
